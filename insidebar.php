67c507e4a4b76207b9e9e1184be0feb0ab17479f
mohamed-sd <sudanit2015@gmail.com>
2025-09-07 00:34:23 +0300

    sidebar icons

--- insidebar.php
+++ insidebar.php
@@ -9,15 +9,15 @@
     <h2 class="logo">Equipation</h2>
 
     <ul>
-      <li><a href="../dashbourd.php"><i class="fa fa-home"></i> <span>الرئيسية</span></a></li>
-      <li><a href="../Projects/projects.php"><i class="fa fa-project-diagram"></i> <span>المشاريع</span></a></li>
-      <li><a href="../Suppliers/suppliers.php"><i class="fa fa-truck"></i> <span>الموردين</span></a></li>
-      <li><a href="../Equipments/equipments.php"><i class="fa fa-tools"></i> <span>الآليات</span></a></li>
-      <li><a href="../Drivers/drivers.php"><i class="fa fa-id-badge"></i> <span>المشغلين</span></a></li>
-      <li><a href="../Oprators/oprators.php"><i class="fa fa-play-circle"></i> <span>التشغيل</span></a></li>
-      <li><a href="../Timesheet/timesheet_type.php"><i class="fa fa-clock"></i> <span>ساعات العمل</span></a></li>
-      <li><a href="../users.php"><i class="fa fa-users"></i> <span>المستخدمين</span></a></li>
-      <li><a href="../Reports/reports.php"><i class="fa fa-chart-line"></i> <span>التقارير</span></a></li>
+      <li><a href="../dashbourd.php"><i class="fa fa-tachometer-alt"></i> <span>الرئيسية</span></a></li>
+      <li><a href="../Projects/projects.php"><i class="fa fa-folder-open"></i> <span>المشاريع</span></a></li>
+      <li><a href="../Suppliers/suppliers.php"><i class="fa fa-truck-loading"></i> <span>الموردين</span></a></li>
+      <li><a href="../Equipments/equipments.php"><i class="fa fa-tractor"></i> <span>الآليات</span></a></li>
+      <li><a href="../Drivers/drivers.php"><i class="fa fa-id-card"></i> <span>المشغلين</span></a></li>
+      <li><a href="../Oprators/oprators.php"><i class="fa fa-cogs"></i> <span>التشغيل</span></a></li>
+      <li><a href="../Timesheet/timesheet_type.php"><i class="fa fa-business-time"></i> <span>ساعات العمل</span></a></li>
+      <li><a href="../users.php"><i class="fa fa-users-cog"></i> <span>المستخدمين</span></a></li>
+      <li><a href="../Reports/reports.php"><i class="fa fa-chart-pie"></i> <span>التقارير</span></a></li>
       <li><a href="../settings.php"><i class="fa fa-cog"></i> <span>الإعدادات</span></a></li>
     </ul>
   </div>
--- sidebar.php
+++ sidebar.php
@@ -1,22 +1,23 @@
 <button class="mobile-menu-btn" id="mobileMenuBtn">
   <i class="fa fa-bars"></i>
 </button>
+
 <div class="sidebar closed" id="sidebar">
   <div>
     <div class="toggle-btn" id="toggleBtn"><i class="fa fa-bars"></i></div>
     <h2 class="logo">Equipation</h2>
  
     <ul>
-      <li><a href="dashbourd.php"><i class="fa fa-home"></i> <span>الرئيسية</span></a></li>
-      <li><a href="Projects/projects.php"><i class="fa fa-project-diagram"></i> <span>المشاريع</span></a></li>
+      <li><a href="dashbourd.php"><i class="fa fa-tachometer-alt"></i>  <span>الرئيسية</span></a></li>
+      <li><a href="Projects/projects.php"><i class="fa fa-folder-open"></i> <span>المشاريع</span></a></li>
       <!-- <li><a href="Contracts/contracts.php"><i class="fa fa-file-contract"></i> <span>العقود</span></a></li> -->
-      <li><a href="Suppliers/suppliers.php"><i class="fa fa-truck"></i> <span>الموردين</span></a></li>
-      <li><a href="Equipments/equipments.php"><i class="fa fa-tools"></i> <span>الآليات</span></a></li>
-      <li><a href="Drivers/drivers.php"><i class="fa fa-id-badge"></i> <span>المشغلين</span></a></li>
-      <li><a href="Oprators/oprators.php"><i class="fa fa-play-circle"></i> <span>التشغيل</span></a></li>
-      <li><a href="Timesheet/timesheet_type.php"><i class="fa fa-clock"></i> <span>ساعات العمل</span></a></li>
-      <li><a href="users.php"><i class="fa fa-users"></i> <span>المستخدمين</span></a></li>
-      <li><a href="Reports/reports.php"><i class="fa fa-chart-line"></i> <span>التقارير</span></a></li>
+      <li><a href="Suppliers/suppliers.php"><i class="fa fa-truck-loading"></i> <span>الموردين</span></a></li>
+      <li><a href="Equipments/equipments.php"><i class="fa fa-tractor"></i> <span>الآليات</span></a></li>
+      <li><a href="Drivers/drivers.php"><i class="fa fa-id-card"></i> <span>المشغلين</span></a></li>
+      <li><a href="Oprators/oprators.php"><i class="fa fa-cogs"></i> <span>التشغيل</span></a></li>
+      <li><a href="Timesheet/timesheet_type.php"><i class="fa fa-business-time"></i> <span>ساعات العمل</span></a></li>
+      <li><a href="users.php"><i class="fa fa-users-cog"></i> <span>المستخدمين</span></a></li>
+      <li><a href="Reports/reports.php"><i class="fa fa-chart-pie"></i> <span>التقارير</span></a></li>
       <li><a href="settings.php"><i class="fa fa-cog"></i> <span>الإعدادات</span></a></li>
     </ul>
   </div>
@@ -45,11 +46,3 @@
     sidebar.classList.toggle('active');
   });
 </script>
-
-<!-- <script>
-  const sidebar = document.getElementById('sidebar');
-  const toggleBtn = document.getElementById('toggleBtn');
-
-  toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle
-</script> -->
